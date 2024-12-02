1fdabdcb7d537f38bb33180dbeb0f71bc5bfa3a6
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2024-12-02 17:23:14 +1000

    Actually working remote debugging

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,1 +1,4 @@
-ssh robot@reset-robot.local 'cd robot && ./.venv/bin/python -m debugpy --listen reset-robot.local:5678 --wait-for-client ./main.py'
+ssh robot@reset-robot.local "echo 'cd ~/robot && nohup ./.venv/bin/python -m debugpy --listen reset-robot.local:5678 --wait-for-client ./main.py' | at now"
+sleep 3
+echo ==========
+echo Task done!
