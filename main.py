6c730593be1f6b21dfd9229f7399248a6128a100
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2024-12-03 12:34:11 +1000

    Robot class with basic movement

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
-def main():
-    print("Hello world!")
+from gpiozero import Servo, AngularServo, Motor  # type: ignore
 
-if __name__ == "__main__":
-    main()
+
+class Robot:
+    manipulator_horizontal = Servo(17)
+    manipulator_grab = AngularServo(15, min_angle=-180, max_angle=180)
+
+    motor_lf = Motor(19, 16)
+    """Мотор впереди слева"""
+
+    motor_rf = Motor(24, 25)
+    """Мотор впереди справа"""
+
+    motor_lb = Motor(14, 13)
+    """Мотор сзади слева"""
+
+    motor_rb = Motor(10, 9)
+    """Мотор сзади справа"""
+
+    def __init__(self) -> None:
+        pass
+
+    def right(self) -> None:
+        self.motor_lf.forward()
+        self.motor_lb.forward()
+
+        self.motor_rf.backward()
+        self.motor_rb.backward()
+
+    def left(self) -> None:
+        self.motor_lf.backward()
+        self.motor_lb.backward()
+
+        self.motor_rf.forward()
+        self.motor_rb.forward()
+
+    def forward(self) -> None:
+        self.motor_lf.forward()
+        self.motor_lb.forward()
+
+        self.motor_rf.forward()
+        self.motor_rb.forward()
+
+    def backward(self) -> None:
+        self.motor_lf.backward()
+        self.motor_lb.backward()
+
+        self.motor_rf.backward()
+        self.motor_rb.backward()
