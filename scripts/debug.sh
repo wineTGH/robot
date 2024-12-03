63471431d4d43c1b5e5b277b5be9cdeed8dca87d
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2024-12-03 10:47:16 +1000

    Deleted nohup from debug run

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,4 +1,4 @@
-ssh robot@reset-robot.local "echo 'cd ~/robot && nohup ./.venv/bin/python -m debugpy --listen reset-robot.local:5678 --wait-for-client ./main.py' | at now"
+ssh robot@reset-robot.local "echo 'cd ~/robot && ./.venv/bin/python -m debugpy --listen reset-robot.local:5678 --wait-for-client ./main.py' | at now"
 sleep 3
 echo ==========
 echo Task done!
