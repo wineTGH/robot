764104e43e0f4af8de9422aedbe2cd6e39bf1204
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2024-12-08 17:50:03 +1000

    server

--- main.py
+++ main.py
@@ -1,85 +1,12 @@
-from gpiozero import Servo, AngularServo, Motor  # type: ignore
-import time
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 
-class Robot:
-    manipulator_horizontal = Servo(17)
-    manipulator_grab = AngularServo(15, min_angle=-180, max_angle=180)
-
-    motor_lf = Motor(19, 16)
-    """Мотор впереди слева"""
-
-    motor_rf = Motor(24, 25)
-    """Мотор впереди справа"""
-
-    motor_lb = Motor(14, 13)
-    """Мотор сзади слева"""
-
-    motor_rb = Motor(10, 9)
-    """Мотор сзади справа"""
-
-    def __init__(self) -> None:
-        pass
-
-    def right(self) -> None:
-        self.motor_lf.forward()
-        self.motor_lb.forward()
-
-        self.motor_rf.backward()
-        self.motor_rb.backward()
-
-    def left(self) -> None:
-        self.motor_lf.backward()
-        self.motor_lb.backward()
-
-        self.motor_rf.forward()
-        self.motor_rb.forward()
-
-    def forward(self) -> None:
-        self.motor_lf.forward()
-        self.motor_lb.forward()
-
-        self.motor_rf.forward()
-        self.motor_rb.forward()
-
-    def backward(self) -> None:
-        self.motor_lf.backward()
-        self.motor_lb.backward()
-
-        self.motor_rf.backward()
-        self.motor_rb.backward()
-
-    def stop(self) -> None:
-        self.motor_lf.stop()
-        self.motor_lb.stop()
-
-        self.motor_rf.stop()
-        self.motor_rb.stop()
-
-
-if __name__ == "__main__":
-    robot = Robot()
-
-    robot.forward()
-    time.sleep(1)
-
-    robot.stop()
-    time.sleep(1)
-
-    robot.backward()
-    time.sleep(1)
-
-    robot.stop()
-    time.sleep(1)
-
-    robot.right()
-    time.sleep(1)
-
-    robot.stop()
-    time.sleep(1)
-
-    robot.left()
-    time.sleep(1)
-
-    robot.stop()
-    time.sleep(1)
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request, name="index.html")
